577a9d2e80fef4a2423823d172aa4849654bb6c0
coil <75682926+ilsung12@users.noreply.github.com>
2021-10-28 21:15:08 +0900

    📝 Learn : UI Library - Matrial UI

---
+++ uilearn/src/MaterialUI/VirtualizedList.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { FixedSizeList } from 'react-window';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    height: 400,
+    maxWidth: 300,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function renderRow(props) {
+  // 컴포넌트 하나씩을 리턴해주는 애
+  const { index, style } = props;
+
+  return (
+    <ListItem button style={style} key={index}>
+      <ListItemText primary={`Item ${index + 1}`} />
+      <Button variant="outlined" color="primary">
+        {index + 1}
+      </Button>
+    </ListItem>
+  );
+}
+
+renderRow.propTypes = {
+  index: PropTypes.number.isRequired,
+  style: PropTypes.object.isRequired,
+};
+
+export default function VirtualizedList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <FixedSizeList height={300} width={300} itemSize={46} itemCount={200}>
+        {renderRow}
+      </FixedSizeList>
+    </div>
+  );
+}
